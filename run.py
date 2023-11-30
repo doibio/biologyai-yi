89a060a17d104a9f5b274f1f6eafd183238273ed
sness <snessnet@gmail.com>
2023-11-30 11:48:26 -0800

    more

---
+++ run.py
@@ -0,0 +1,42 @@
+import requests
+import os
+import codecs
+
+
+API_URL = "https://zbcr1xtvyhjfqg93.us-east-1.aws.endpoints.huggingface.cloud"
+headers = {
+	"Authorization": "Bearer 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",
+	"Content-Type": "application/json"
+}
+
+
+directory_path = 'edirect2'
+
+documents = []
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+    
+for filename in os.listdir(directory_path):
+    if filename.endswith(".txt"):  
+        filepath = os.path.join(directory_path, filename)
+        try:
+            with codecs.open(filepath, 'r', encoding='utf-8') as file:
+                content = file.read()
+                
+                document = {
+                    "title": filename.replace('.txt', ''),  
+                    "snippet": content
+                }
+                documents.append(document)
+        except Exception as e:
+            print(f"Error reading {filename}: {e}")
+
+
+for document in documents:
+    output = query({
+	"inputs": "Please summarize the following: " + document,
+    })
+    print("----------------------------------------")
+    print(output)
--- test-yi.py
+++ test-yi.py
@@ -11,7 +11,7 @@ def query(payload):
 	return response.json()
 	
 output = query({
-	"inputs": "Please summarize the following: Erikson’s Stages of Psychosocial Development is a theory introduced in the 1950s by the psychologist and psychoanalyst Erik Erikson. It built upon Freud’s theory of psychosexual development by drawing parallels in childhood stages while expanding it to include the influence of social dynamics as well as the extension of psychosocial development into adulthood. It posits eight sequential stages of individual human development influenced by biological, psychological, and social factors throughout the lifespan. This bio-psychosocial approach has influenced several fields of study, including gerontology, personality development, identity formation, life cycle development, and more.",
+	"inputs": "Please summarize the following: ",
 })
 
 print(output)
